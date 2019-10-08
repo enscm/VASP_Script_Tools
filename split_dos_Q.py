e1d1544d1e520545157551f522275307ced5e664
In the fly <50736751+enscm@users.noreply.github.com>
2019-10-08 18:04:09 +0200

    code for splitting DOSCAR
    This code will split the DOS for every atom in the system from DOSCAR, and named as DOS0 (the total dos for all atoms),DOS1 (dos for atom number 1), DOS2 (dos for atom 2),ect...

--- split_dos_Q.py
+++ split_dos_Q.py
@@ -6,7 +6,12 @@ import pandas as pd
 ##--------------------------------------------------------------------
 ##   code writen on 20 sept 2019, by Qing W
 ##            qing.wang@outlook.fr
-##This only works for spin-polarized DOS calc. (LORBIT = 11, ICHARGE= 11)
+##
+## This only works for spin-polarized DOS calc. (LORBIT = 11, ICHARGE= 11)
+## The fermi-lever has been returned to zero in this code
+## Hint: This code will split the DOS for every atom in the system from
+##       DOSCAR, and named as DOS0 (the total dos for all atoms),
+##       DOS1 (dos for atom number 1), DOS2 (dos for atom 2),ect...
 ##--------------------------------------------------------------------
 ##
 ##       \   ,__,
@@ -22,6 +27,7 @@ import pandas as pd
 
 f = open('DOSCAR', 'r')
 lines = f.readlines()
+print(type(lines))
 f.close()
 index = 0
 natoms = int(lines[index].strip().split()[0])
@@ -31,7 +37,6 @@ efermi = float(lines[index].strip().split()[3])
 print("WARNING: only tested for 'vasp.4.*', LORBIT=11, Spin polarized calc, use with caution! " )
 print("There are",natoms,"atoms in system")
 print("Fermi lever is",efermi,"eV")
-
 
 ### write DOS0 ####
 with open("DOS0","w+") as writer0:
@@ -43,7 +48,7 @@ with open("DOS0","w+") as writer0:
 
 ### here loop in loop, for the second loop, it will run all possibility(statements) firstly,
         # then will return to first loop, then try all possibily in second loop agin###
-        for j in range(1,5):
+        for j in range(1,5,2):
             tot = float(lines[i].strip().split()[j])
             writer0.writelines('%15.8f'%(tot))
         writer0.write('\n')
@@ -59,13 +64,8 @@ for i in range(1,natoms+1):
         en_ef = en - efermi
         fdos.writelines('%15.8f' % (en_ef))
 
-        for j in range(1,19):
+        for j in range(1,19,2):
             line = float(lines[i].strip().split()[j])
             fdos.writelines('%15.8f' % (line))
         fdos.write('\n')
 fdos.close()
-
-
-
-
-
